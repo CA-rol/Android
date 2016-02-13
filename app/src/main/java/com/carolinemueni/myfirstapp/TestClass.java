e7a149ff42d2f30384a26c5be5dad6aa4c05b868
Student <Student>
2016-02-13 16:17:59 +0300

    first commit

---
+++ app/src/main/java/com/carolinemueni/myfirstapp/TestClass.java
@@ -0,0 +1,25 @@
+package com.carolinemueni.myfirstapp;
+
+/**
+ * Created by Student on 2/6/2016.
+ */
+public class TestClass {
+    //declare a variable
+
+    public static void main(String args[]){
+        int age;
+        double shoesize;
+        char letter;
+        String firstName;
+
+        //initialize the variables
+        age = 10;
+        shoesize = 5.5;
+        letter ='A';
+        firstName = "Mueni Anne";
+
+        System.out.print(age);
+
+    }
+
+}
---
+++ app/src/main/java/com/carolinemueni/myfirstapp/maths.java
@@ -0,0 +1,35 @@
+package com.carolinemueni.myfirstapp;
+
+/**
+ * Created by Student on 2/6/2016.
+ */
+public class maths {
+    public static void main(String args[]) {
+
+        //call a method by  the method  name
+        addition();
+        subtraction();
+    }
+
+        //addition method
+        public static void addition() {
+
+        int age = 24;
+        int height = 6;
+        int answer = age + height;
+
+        System.out.println(answer);
+
+    }
+        //subtraction method
+        public static void subtraction() {
+            int myrose = 5;
+            int width = 3;
+            int answer = myrose - width;
+
+            System.out.println(answer);
+        }
+    //multiplication method
+
+
+}
